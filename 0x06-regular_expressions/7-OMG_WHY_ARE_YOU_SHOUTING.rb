f4460e431ff1c281e570929f31718f07b5cf6215
steve9948 <mbuguagichimu@gmail.com>
2023-11-28 06:43:23 -0800

    OMG stop shouting

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# A regular expressions matching capital letters
+# A regular expressions matching uppercase letters
 
-puts ARGV[0].scan(/^[A-Z]*$/).join
+puts ARGV[0].scan(/[A-Z]+/).join
